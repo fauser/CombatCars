05803167710e7f8c36c695991ee5c3a3dc524cbb
Daniel Svensson <fauser78@gmail.com>
2013-04-14 17:27:39 +0200

    Testproject

---
+++ CombatCarsAPITest/RSATest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using CombatCarsAPI.Security;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CombatCarsAPITest
+{
+    [TestClass]
+    public class RSATest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var token = "Daniel";
+            var encryptedToken = RSAClass.Encrypt(token);
+            var decryptedToken = RSAClass.Decrypt(encryptedToken);
+
+            Assert.AreEqual(token, decryptedToken);
+        }
+    }
+}
